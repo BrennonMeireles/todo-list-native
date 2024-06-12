752f642dfc87989ba0ff792b9f4866315e5897b3
Brennon <brennonmeireles7@gmail.com>
2024-06-12 11:06:20 -0300

    feat: created app

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState, useEffect } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
+import Picker from "react-native-picker";
+ 
+import NotaEditor from "./src/components/NotaEditor";
+import Nota from "./src/components/Nota";
+import { criarTabela, buscarNotas } from "./src/services/Notas";
+ 
 export default function App() {
+  const [notas, setNotas] = useState([]);
+  const [notaSel, setNotaSel] = useState({});
+  const [categoria, setCategoria] = useState("*");
+ 
+  useEffect(() => {
+    criarTabela();
+  }, []);
+ 
+  useEffect(() => {
+    async function iniciarNotas() {
+      await mostrarNotas();
+    }
+    iniciarNotas();
+  }, [categoria]);
+ 
+  async function mostrarNotas() {
+    const tarefas = await buscarNotas(categoria);
+    setNotaSel(tarefas);
+  }
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.modalPicker}>
+        <Picker
+          selectedValue={categoria}
+          onValueChanged={(itemValue) => setCategoria(itemValue)}
+        >
+          <Picker.Item Label="Pessoal" value="Pessoal" />
+          <Picker.Item Label="Trabalho" value="Trabalho" />
+          <Picker.Item Label="Outros" value="Outros" />
+        </Picker>
+      </View>
+      <FlatList
+        data={notas}
+        renderItem={(nota) => <Nota item={nota.item} setNotaSel={setNotaSel} />}
+        keyExtractor={(notas) => nota.id}
+      />
+      <NotaEditor
+        showNotas={mostrarNotas}
+        notaSel={notaSel}
+        setNotaSel={setNotaSel}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
+ 
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+  },
+  modalPicker: {
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: "#eee",
+    margin: 10,
   },
 });
+ 
